e5f758d05d1cf5bb3975193e30d5deb77a688d36
Bogdan Bogdanov <bogdanovbogdanv@gmail.com>
2026-03-04 13:25:09 +0700

    Add setup logger to url-shortener

--- url-shortener/cmd/main.go
+++ url-shortener/cmd/main.go
@@ -1,9 +1,43 @@
 package main
 
 import (
+	"log/slog"
+	"os"
 	"url-shortener/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.LoadConfig()
+
+	logger := setupLogger(cfg.Env)
+
+	logger.Info("info log", slog.String("env", cfg.Env))
+	logger.Debug("debug log")
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
 }
